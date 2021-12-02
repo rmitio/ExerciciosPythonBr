f7d495959cc6b06743181a92e1c738c0c7640f6b
Rafael Mitio <rafaelmitiof@gmail.co0m>
2021-12-02 12:01:00 -0300

    ex3 incompleto

---
+++ exercicio3.py
@@ -0,0 +1,5 @@
+#Faça um Programa que peça dois números e imprima a soma.
+
+numero1 = input('Digite um número: \n')
+numero2 = input('Digite outro número: \n')
+print(f'A soma dos númerod digitados é: {numero1+numero2}')
