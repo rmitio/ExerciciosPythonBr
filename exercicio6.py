1a2fb6d188a0bd7ef5c523727d898e706339342f
Rafael Mitio <rafaelmitiof@gmail.co0m>
2021-12-02 15:47:40 -0300

    Ex 6

---
+++ exercicio6.py
@@ -0,0 +1,5 @@
+#Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+#area = 3.14 * (raio*raio)
+raio = int(input('Digite o valor do Raio: '))
+area = 3.14 * (raio*raio)
+print(f'A área do circulo é {area}')
