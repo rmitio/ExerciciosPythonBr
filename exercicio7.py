22428583939f4a55b284cbbcdb73f02d5e70e277
Rafael Mitio <rafaelmitiof@gmail.co0m>
2021-12-03 10:28:45 -0300

    Ex 7

---
+++ exercicio7.py
@@ -0,0 +1,6 @@
+#Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+#A = b. h
+
+b = int(input('Digite a altura do quadrado: '))
+
+print(f'A Area do quadrado é {b * b}')
