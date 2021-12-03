7624425b8bf38ee936856332d0663a040d4683e1
Rafael Mitio <rafaelmitiof@gmail.co0m>
2021-12-03 10:57:52 -0300

    Ex 10 e Ex11

---
+++ exercicio10.py
@@ -0,0 +1,8 @@
+#Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
+#F = C * 1.8 + 32
+
+C = int(input('Digite a temperatura em Celsius: '))
+F = C * 1.8 + 32
+
+print(f'{C}ºC é {F}º F')
+
