f60d753ee24eda19907cb8cbf5b5e134122da4a8
Rafael Mitio <rafaelmitiof@gmail.com>
2021-12-06 10:46:43 -0300

    Ex 14

---
+++ exercicio14.py
@@ -0,0 +1,9 @@
+#João Papo-de-Pescador, homem de bem, comprou um microcomputador para controlar o rendimento diário de seu trabalho. Toda vez que ele traz um peso de peixes maior que o estabelecido pelo regulamento de pesca do estado de São Paulo (50 quilos) deve pagar uma multa de R$ 4,00 por quilo excedente. João precisa que você faça um programa que leia a variável peso (peso de peixes) e calcule o excesso. Gravar na variável excesso a quantidade de quilos além do limite e na variável multa o valor da multa que João deverá pagar. Imprima os dados do programa com as mensagens adequadas.
+peso = float(input('Digite o peso: '))
+excesso = peso - 50
+multa = excesso * 4
+
+if excesso <= 0:
+    print('Não tem excessos')
+else:
+    print(f'Você tem {excesso} kgs a mais e deve pagar R$ {multa:.2f} pelo excedente.')
